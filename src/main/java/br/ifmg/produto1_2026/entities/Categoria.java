8f45815310792a3a0e6d9f75a9a752da450d97ec
Gabriel Castro <gabrielcastrogundin@gmail.com>
2026-03-03 11:38:02 -0300

    02 - criando entidade

--- src/main/java/br/ifmg/produto1_2026/entities/Categoria.java
+++ src/main/java/br/ifmg/produto1_2026/entities/Categoria.java
@@ -1,8 +1,15 @@
 package br.ifmg.produto1_2026.entities;
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 
+
+@Table(name = "tb_categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
--- src/main/java/br/ifmg/produto1_2026/resources/categoriaResource.java
+++ src/main/java/br/ifmg/produto1_2026/resources/categoriaResource.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/categorias")
 public class categoriaResource {
     @GetMapping
-    public ResponseEntity<Categoria> categoria() {
+    public ResponseEntity<List<Categoria>> categoria() {
         Categoria categoria = new Categoria(1L, "notebook");
-        return ResponseEntity.ok().body(categoria);
+        Categoria categoria1 = new Categoria(2L, "celular");
+        Categoria categoria2 = new Categoria(3L, "livro");
+        Categoria categoria3 = new Categoria(4L, "fones");
+        ArrayList<Categoria> categorias = new ArrayList<>();
+        categorias.add(categoria);
+        categorias.add(categoria1);
+        categorias.add(categoria2);
+        categorias.add(categoria3);
+        return ResponseEntity.ok().body(categorias);
     };
-
 }
